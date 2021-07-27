c240c8d2b4d29a19e257545e359088ba9ba36e87
CristhianMotoche <cristhian.motoche@gmail.com>
2021-07-27 17:29:49 -0500

    Add unit tests for down

--- Elm/tests/LibraryTest.elm
+++ Elm/tests/LibraryTest.elm
@@ -3,6 +3,7 @@ module LibraryTest exposing (..)
 import Test exposing (..)
 import Expect as E
 import Library as L
+import Fuzz as F
 
 upSpec : Test
 upSpec =
@@ -16,4 +17,18 @@ upSpec =
   , test "returns start when list is small" <|
     \_ ->
       L.up 0 [1, 2] |> E.equal 0
+  , fuzz2 F.int (F.list F.int)"up start list >= start" <|
+    \start ls ->
+        L.up start ls |> E.atLeast start
+  ]
+
+downSpec : Test
+downSpec =
+  describe "down"
+  [ test "returns 0 when start is 1 and list has N elements" <|
+    \_ ->
+      L.down 1 |> E.equal 0
+  , fuzz F.int "down start <= start" <|
+    \start ->
+        L.down start |> E.atMost start
   ]
