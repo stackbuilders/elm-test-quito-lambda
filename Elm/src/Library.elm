1fa5383417cc6ce1d4ad418df47e3ee360bbac37
CristhianMotoche <cristhian.motoche@gmail.com>
2021-07-26 21:26:07 -0500

    Add view

--- Elm/src/Library.elm
+++ Elm/src/Library.elm
@@ -1,5 +1,8 @@
 module Library exposing (..)
 
+import Html as H
+import Html.Events as HE
+
 -- Model
 type alias Model =
   { books: List Book
@@ -19,3 +22,40 @@ type alias Book =
 type Msg
   = Next
   | Prev
+
+
+-- View
+view : Model -> H.Html Msg
+view model =
+  H.div
+  []
+  <| List.map viewBook (slice model.start (model.start + 3) model.books) ++ [viewActions]
+
+viewActions : H.Html Msg
+viewActions =
+    H.div
+    []
+    [ H.button [ HE.onClick Prev  ][ H.text "Prev"  ]
+    , H.button [ HE.onClick Next  ][ H.text "Next"  ]
+    ]
+
+viewBook : Book -> H.Html Msg
+viewBook book =
+    H.div
+    []
+    [ H.h1 [][ H.text book.title  ]
+    , H.p [][ H.text book.synopsis  ]
+    , H.i [][ H.text <| "By "++ authorToString book.author  ]
+    ]
+
+authorToString : AuthorName -> String
+authorToString author =
+  case author of
+    Name name -> name
+    Anonymous -> "Anonymous"
+
+slice : Int -> Int -> List a -> List a
+slice start end list =
+  List.drop start
+  <| List.take end
+  <| list
