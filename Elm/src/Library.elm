890324dbe9241403178b47e164cbe81563755394
CristhianMotoche <cristhian.motoche@gmail.com>
2021-07-26 21:19:07 -0500

    Remove fixture data

--- Elm/src/Library.elm
+++ Elm/src/Library.elm
@@ -16,33 +16,6 @@ type alias Book =
   , author : AuthorName
   }
 
-init : Model
-init =
-  { books = [
-    { title = "Iliada"
-    , synopsis = "La batalla entre aqueos y troyanos"
-    , author = Name "Homero"
-    },
-    { title = "Odisea"
-    , synopsis = "Las aventuras de Odiseo en su camino a Ítaca desde Troya"
-    , author = Name "Homero"
-    },
-    { title = "El libro que nunca escribí"
-    , synopsis = "Habla sobre las cosas que nunca dije y las letras que no escribí"
-    , author = Anonymous
-    },
-    { title = "La rebelión de la granja"
-    , synopsis = "¿Cómo terminará la rebelión de los animales?"
-    , author = Name "George Orwell"
-    },
-    { title = "Un mundo feliz"
-    , synopsis = "El mundo ideal donde todos son felices, ¿o no?"
-    , author = Name "Aldous Huxley"
-    }
-  ]
-  , start = 0
-  }
-
 type Msg
   = Next
   | Prev
